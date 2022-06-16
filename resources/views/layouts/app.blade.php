2a2328a9cda68f4d888e060a2a5d004118e956f7
Isaac Anane Amankwaah <57358941+brasmith9@users.noreply.github.com>
2022-06-16 15:04:54 +0000

    Vercel are you ready😎

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
   <!-- HTML Meta Tags -->
-  <title>Isaac Anane | FullStack Developer</title>
+  <title>Isaac Anane Amankwaah| FullStack Developer</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="title" content="Isaac Anane Amankwaah—Fullstack developer">
   <meta name="keywords"
@@ -53,7 +53,7 @@
       <div class="nav">
         <div class="nav__brand__container">
           <div class="nav__brand">
-            <a href="{{route('home')}}"><img src="img/logo.png" alt="monogram" height="24"
+            <a href="{{route('home')}}"><img src="img/favicon/favicon.ico" alt="monogram" height="24"
                 style="margin-right: 0.5rem" />developer.anane</a>
           </div>
           <div class="nav__opener">
@@ -88,6 +88,43 @@
     {{ $slot }}
   </main>
 
+  <section class="section contact-me" id="contact-me">
+    <article class="mycontainer">
+      <h2 class="">Contact Me</h2>
+      <form class="contact-me__form">
+        <section class="form-element">
+          <label for="name">Name</label>
+          <input type="text" id="name" name="name" required>
+        </section>
+        <section class="form-element">
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" required>
+        </section>
+        <section class="form-element">
+          <label for="message">Message</label>
+          <textarea name="message" id="message" required></textarea>
+        </section>
+        <section class="form-element">
+          <button title="Send Message" type="submit" class="btn btn--primary">
+            <figure><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32"
+                viewBox="0 0 172 172" style=" fill:#000000;">
+                <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                  stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                  font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                  style="mix-blend-mode: normal">
+                  <path d="M0,172v-172h172v172z" fill="none"></path>
+                  <g fill="#ffffff">
+                    <path
+                      d="M42.05255,30.98236c-3.12684,0.08083 -6.02325,1.65502 -7.78693,4.35931c-2.01562,3.09063 -1.88178,6.9875 0.13385,10.07813l11.6922,18.00415c1.20938,1.88125 3.76198,2.41927 5.50885,1.2099c1.88125,-1.20937 2.41927,-3.76197 1.2099,-5.50885l-11.6922,-18.00678c-0.40313,-0.67187 -0.13437,-1.20885 0,-1.4776c0.13437,-0.26875 0.5375,-0.67187 1.34375,-0.67187l118.92188,14.78125c0.67188,0.13437 0.94167,0.53645 1.07605,0.93958c0.13438,0.40312 0.26718,0.80625 -0.27032,1.34375l-81.56458,87.61407c-0.5375,0.5375 -1.075,0.40155 -1.34375,0.40155c-0.26875,-0.13438 -0.8052,-0.40155 -0.93958,-1.07343l-3.49585,-27.54687c-0.13438,-0.80625 -0.13333,-1.7474 -0.2677,-2.55365c-0.5375,-12.9 7.25625,-24.5901 19.48438,-29.02447l26.06928,-9.40625c2.15,-0.80625 3.22343,-3.09168 2.41718,-5.1073c-0.80625,-2.15 -3.08905,-3.22343 -5.10468,-2.41718l-26.06928,9.40625c-16.6625,6.04688 -26.74167,22.5729 -24.59167,40.17603l3.49585,27.54688c0.40313,3.62813 2.95468,6.58542 6.44843,7.7948c0.94062,0.26875 2.0172,0.40417 2.95782,0.40417c2.55312,0 4.97135,-1.07657 6.8526,-2.95782l81.56458,-87.61145c2.41875,-2.55313 3.225,-6.3172 2.01563,-9.67657c-1.20937,-3.35937 -4.16458,-5.64323 -7.65833,-6.18073l-119.05572,-14.78125c-0.45352,-0.05039 -0.90493,-0.06929 -1.35162,-0.05774zM49.37232,81.83752c-0.51021,-0.021 -1.03143,0.06351 -1.53534,0.26507l-25.26093,9.13855c-2.15,0.80625 -3.22605,3.08905 -2.4198,5.10468c0.5375,1.74687 2.01667,2.6875 3.76355,2.6875c0.40313,0 0.94063,-0.13333 1.34375,-0.2677l25.26093,-9.13855c2.15,-0.80625 3.22605,-3.08905 2.4198,-5.10468c-0.60469,-1.6125 -2.04135,-2.62189 -3.57196,-2.68488zM55.68689,99.5766c-0.5102,-0.021 -1.02881,0.06351 -1.53271,0.26507l-42.86615,15.58698c-2.15,0.80625 -3.22605,3.08905 -2.4198,5.10468c0.5375,1.6125 2.15105,2.6875 3.76355,2.6875c0.40313,0 0.94063,-0.13333 1.34375,-0.2677l42.86615,-15.58697c2.15,-0.80625 3.22343,-3.09168 2.41718,-5.1073c-0.60469,-1.6125 -2.04134,-2.61926 -3.57196,-2.68225z">
+                    </path>
+                  </g>
+                </g>
+              </svg></figure>Send
+          </button>
+        </section>
+      </form>
+    </article>
+  </section>
   <footer>
     <div class="mycontainer bar">
       <div class="copyright">&copy; 2022, Isaac Anane Amankwaah. All rights reserved.</div>
